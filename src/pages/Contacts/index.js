2f46048c4f6370a240892b46504f34573ff63fd4
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2020-11-01 14:25:43 +0200

    add margin styles for container

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,40 +1,22 @@
-import { useState, useEffect } from "react";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { useContacts } from "./useContacts";
 
-const useContacts = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    const getContacts = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch("https://randomuser.me/api/?results=200");
-        const { results, error } = await response.json();
-        if (error) {
-          throw new Error(error);
-        }
-        setData(results);
-        setIsError(false);
-      } catch (e) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    getContacts();
-  }, []);
-
-  return {
-    data,
-    isLoading,
-    isError,
-  };
-};
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      marginTop: theme.spacing(4),
+    },
+    headContainer: {
+      marginBottom: theme.spacing(3),
+    },
+  })
+);
 
 export const Contacts = () => {
+  const classes = useStyles();
   const contacts = useContacts();
 
   if (contacts.isLoading) {
@@ -46,10 +28,15 @@ export const Contacts = () => {
   }
 
   return (
-    <Container>
+    <Container className={classes.root}>
       <Grid container>
+        <Grid item xs={12} className={classes.headContainer}>
+          <Typography variant="h3" component="h1">
+            Contacts
+          </Typography>
+        </Grid>
         <Grid item xs={12}>
-          <div>Contacts {contacts.data[0].name.first}</div>
+          <div>{contacts.data[0].name.first}</div>
         </Grid>
       </Grid>
     </Container>
