83a03b77fb4eabcc3a53cf926464685b82b9faf5
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2020-11-01 11:28:53 +0200

    load contacts from api

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,31 @@
+import { useState, useEffect } from "react";
+
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  const [contacts, setContacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://randomuser.me/api/?results=200")
+      .then((response) => response.json())
+      .then(({ results }) => {
+        setContacts(results);
+        setIsLoading(false);
+        setIsError(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+        setIsError(true);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div>...loading</div>;
+  }
+
+  if (isError) {
+    return <div>...error</div>;
+  }
+  return <div>Contacts {contacts[0].name.first}</div>;
 };
